7878c8a04fbccec82ed1dd425f291866f650fe89
Rafaela Tillmann <rafaelatillmann.rt@gmail.com>
2025-07-15 09:42:50 -0300

    feat: progress with warehouse

--- db-go/crud/internal/domain/repository.go
+++ db-go/crud/internal/domain/repository.go
@@ -6,3 +6,10 @@ type RepositoryProduct interface {
 	Update(p *Product) (err error)
 	Delete(id int) (err error)
 }
+
+type RepositoryWarehouse interface {
+	FindById(id int) (p Warehouse, err error)
+	Save(p *Warehouse) (err error)
+	Update(p *Warehouse) (err error)
+	Delete(id int) (err error)
+}
---
+++ db-go/crud/internal/domain/warehouse.go
@@ -0,0 +1,4 @@
+package domain
+
+type Warehouse struct {
+}
